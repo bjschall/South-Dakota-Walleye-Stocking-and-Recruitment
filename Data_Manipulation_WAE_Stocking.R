9c3a1de2eed8c5477031240b639bcd96d625ac2e
bjschall <89793677+bjschall@users.noreply.github.com>
2024-12-27 12:41:12 -0600

    Update Data_Manipulation_WAE_Stocking.R

--- Data_Manipulation_WAE_Stocking.R
+++ Data_Manipulation_WAE_Stocking.R
@@ -3,8 +3,8 @@ library(magrittr)
 library(dplyr)
 library(Matrix)
 library(tidyverse)
-wae<-read_excel("WAE Stocking Analysis Final 2-2-21.xlsx", sheet="CleanData")
-wae%<>%mutate(CPUE2=YC2.conv2GFP,County=as.factor(County),
+wae<-read_excel("WAE Stocking Analysis Final Data.xlsx", sheet="Walleye")
+wae%<>%mutate(County=as.factor(County),
               Waterbody = as.factor(Waterbody), 
               Size=as.factor(Size), 
               ManagementPurpose=as.factor(ManagementPurpose),
@@ -13,23 +13,20 @@ wae%<>%mutate(CPUE2=YC2.conv2GFP,County=as.factor(County),
               Stocked=as.factor(Stocked),
               Hectares=Acres*0.404686) %<>%
   mutate(ManagementPurpose=plyr::mapvalues(ManagementPurpose,from="Winterkill", to="Introductory"))
-wae<-droplevels(filter(wae, !is.na(YC2.conv2GFP)&Size!="Multiple"&Size!="Adult"&Size!="Juvenile"&Size!="Fingerling"|is.na(Size)))
-wae2<-droplevels(filter(wae, !is.na(YC2.conv2GFP)))
-wae2 %<>% mutate(CPUE_adj=(YC2.conv2GFP+0.1),
-                 Size2=as.character(as.numeric(Size))) %>% 
+wae2<-droplevels(filter(wae, Size!="Multiple"&Size!="Adult"&Size!="Juvenile"&Size!="Fingerling"|is.na(Size)))
+wae2 %<>% mutate(Size2=as.character(as.numeric(Size))) %>% 
   mutate(Size2=plyr::mapvalues(Size2, from=c("1","2", "3", "4"),
                          to=c("d_fr","b_lf","a_no","c_sf"))) %>% 
   mutate(Size2=as.factor(Size2),
          Waterbody2=group_indices(wae2, .dots=c("Waterbody", "County")),
          Centrarchids=BLC.S+BLG.S,
+         WAE.All_conv=ifelse(GNType_cohort.year=="std exp gill net", WAE.All, (10^(0.798*(log10(WAE.All+1))+0.424))-1), 
+         WAE.S_conv=ifelse(GNType_cohort.year=="std exp gill net", WAE.S, (10^(0.798*(log10(WAE.S+1))+0.424))-1),
          Type=ifelse(Waterbody=="Antelope"|Waterbody=="Bitter"| Waterbody=="Blue Dog"|Waterbody=="Brant"|Waterbody=="Brush"|Waterbody=="Buffalo South"|
                        Waterbody=="Clear"&County=="Hamlin"|Waterbody=="Clear"&County=="Marshall"|Waterbody=="Dry 2"|Waterbody=="Enemy Swim"|
                        Waterbody=="Goose"|Waterbody=="Hazeldon"|Waterbody=="Horseshoe"|Waterbody=="Hwy 81 West"|Waterbody=="Kampeska"| Waterbody=="Long"|
                        Waterbody=="Lynn"|Waterbody=="Madison"|Waterbody=="Middle Lynn"|Waterbody=="Opitz"|Waterbody=="Pickerel"|Waterbody=="Piyas"|Waterbody=="Poinsett"|
-                       Waterbody=="Reetz"|Waterbody=="Reid"|Waterbody=="Sinai"|Waterbody=="Thompson"|Waterbody=="Twin"&County=="Minnehaha"|Waterbody=="Waubay", "Consistent","Marginal")) %>% 
-  select(-WAE.All,-WHB.All,-BLB.All, -COC.All,-WHS.All, -YEP.All,-YEP.psd,-YEP.psdp,
-         -Region,-YC3.conv2GFP,-GNType,-YC3.GNType, -YC3, -COC.psd,-COC.psdp,-WHS.psd,-WHS.psdp, -CPUE3,
-         -BLB.psd,-BLB.psdp,-WHB.psd,-WHB.psdp,L1,L3,L4,L5,L6,L7,L8,L9,L10, N1,N3,N4,N5,N6,N7,N8,N9,`N10+`)
+                       Waterbody=="Reetz"|Waterbody=="Reid"|Waterbody=="Sinai"|Waterbody=="Thompson"|Waterbody=="Twin"&County=="Minnehaha"|Waterbody=="Waubay", "Consistent","Marginal"))
 #add NOP data
 nop<-read.csv("Nop.csv")
 
@@ -44,8 +41,8 @@ nop2<-nop %>%
 waenop<-nop2 %>% select(-Station) %>% 
   right_join(wae2, by=c("County", "Waterbody", "Year.cohort"))
 
-wae2.1<- waenop %>% mutate(NOP.All.raw=total_nop/NbofGN,
-                           NOP.S.raw = total_nop_stock/NbofGN) %>% 
+wae2.1<- waenop %>% mutate(NOP.All.raw=total_nop/NbGN_cohort.year,
+                           NOP.S.raw = total_nop_stock/NbGN_cohort.year) %>% 
   mutate(NOP.All = ifelse(Method=="std exp gill net", NOP.All.raw, (10^(1.187*(log10(NOP.All.raw+1))+0.135))-1),
          NOP.S = ifelse(Method=="std exp gill net", NOP.S.raw, (10^(1.187*(log10(NOP.S.raw+1))+0.135))-1)) %>% 
   select(-NOP.All.raw, -NOP.S.raw, -Method); wae2.1
@@ -57,7 +54,10 @@ write.csv(wae2.1, "wae2.1.csv")
 
 ##
 wae2.2<-wae2.1 %>% 
-  filter(SpeciesStocked != "Saugeye") 
+  filter(SpeciesStocked != "Saugeye"&
+           Effort_Age2CatchYear>2&
+           ManagementPurpose!="Introductory"&
+           WaterbodyType=="Natural Basin") 
 wae.final<- wae2.2 %>% ungroup() %>% 
   mutate(GDD_std=(as.numeric(GDD)-mean(as.numeric(GDD)))/sd(as.numeric(GDD)),
          SpringPrecip_std=(SpringPrecip-mean(SpringPrecip))/sd(SpringPrecip),
